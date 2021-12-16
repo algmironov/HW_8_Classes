efbbfae5885e00185184a5cc79b236d50395f549
algmironov <al.g.mironov@yandex.ru>
2021-12-16 22:22:32 +0300

    Объекты и классы

--- src/ru/skystudent/Main.java
+++ src/ru/skystudent/Main.java
@@ -7,9 +7,11 @@ public class Main {
         Author kenKouzen = new Author("Кен", "Коузен");
         Book javaForAll = new Book("Java для всех", alexeyVasilev, 2019);
         Book modernJava = new Book("Современный Java", kenKouzen, 2019);
+        modernJava.setYearOfPublishing(2017);
         System.out.println(modernJava.getAuthor());
         System.out.println(modernJava.getName());
         System.out.println(javaForAll.getName());
         System.out.println(javaForAll.getAuthor());
+        System.out.println(modernJava.getYearOfPublishing());
     }
 }
